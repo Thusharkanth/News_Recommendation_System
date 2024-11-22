201f1b1a77e05b764595ef5240cabf3f5beeb896
Thusharkanth <thusharkanth2002@gmail.com>
2024-11-22 22:22:05 +0530

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.ood_coursework {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.ood_coursework to javafx.fxml;
+    exports com.example.ood_coursework;
+}
